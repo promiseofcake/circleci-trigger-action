1b0ea86169ca3995a7d5dca73dd5cc32728723fc
Lucas Kacher <lucas@vsco.co>
2020-07-22 22:54:29 -0700

    feat: build action

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const axios = require('axios');
+const util = require('util');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const branch = core.getInput('branch');
+    const payload = core.getInput('payload');
+    const platform = core.getInput('platform');
+    const repository = core.getInput('repository');
+    const token = core.getInput('token');
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const jsonObj = JSON.parse(payload)
 
-    core.setOutput('time', new Date().toTimeString());
+    var requestPayload = {
+      branch: branch,
+      parameters: jsonObj
+    };
+
+    let headers = {
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          "Circle-Token": token,
+      }
+    };
+
+    let url = util.format(
+      'https://circleci.com/api/v2/project/%s/%s/pipeline',
+      platform,
+      repository
+    );
+
+    axios.post(url, requestPayload, headers)
+    .then((res) => {
+      console.log("RESPONSE: ", res);
+    })
+    .catch((err) => {
+      console.log("ERROR: ", err);
+      throw err;
+    })
+
   } catch (error) {
     core.setFailed(error.message);
   }
